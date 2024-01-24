25d57f91258df31da32f99c5410679a8a7a42f7a
Prashant170620 <ttipathiprashant300@gmail.com>
2024-01-24 20:37:26 +0530

    Last commit

--- server.js
+++ server.js
@@ -4,16 +4,14 @@ const fs = require('fs');
 var app = express();
 
 app.get('/',(req,res)=>{
-    // fs.readFile('public/index.html',(er,da)=>{
-    //     if(er){
-    //         res.json(er);
-    //     }
-    //     else{
-    //         res.send(da.toString());
-    //     }
-    // })  
-    
-    res.send('Hello World');
+    fs.readFile('public/index.html',(er,da)=>{
+        if(er){
+            res.json(er);
+        }
+        else{
+            res.send(da.toString());
+        }
+    })  
 });
 
 app.listen(3000,()=>{
