4ad47d950056b5dfc632d908f814665db34da84d
Prashant170620 <ttipathiprashant300@gmail.com>
2024-01-24 20:19:45 +0530

    No

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const fs = requrie('fs');
 var app = express();
 
 app.get('/',(req,res)=>{
-    fs.readFile('public/index.html',(er,da)=>{
-        if(er){
-            res.json(er);
-        }
-        else{
-            res.send(da.toString());
-        }
-    })    
+    // fs.readFile('public/index.html',(er,da)=>{
+    //     if(er){
+    //         res.json(er);
+    //     }
+    //     else{
+    //         res.send(da.toString());
+    //     }
+    // })  
+    
+    res.send('Hello World');
 });
 
 module.exports = app;
