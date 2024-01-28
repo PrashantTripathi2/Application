9a9c4d7e7c0f20a9cc36222671dca419caadf77a
Prashant170620 <ttipathiprashant300@gmail.com>
2024-01-29 04:33:44 +0530

    All

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { KVNamespace } = require('@vercel/node');
-const kv = new KVNamespace('ecfg_4osz0xezgwcrngstdh1psqgyh2is');
+const { get } = require('@vercel/edge-config');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,8 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/app',(req,res)=>{
-    var data = kv.get("greeting");
-    res.send(`Data from data base ${data}`);  
+    res.send(`Data from data base ${get("greeting")}`);  
 })
 
 app.get('*', (req, res) => {
