d0f51b61a7f7f0c22d6a25ec7a070a960cca2f0b
Prashant170620 <ttipathiprashant300@gmail.com>
2024-01-29 03:57:27 +0530

    All

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/app',(req,res)=>{
+    res.send('Hello This is from app');  
+})
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
