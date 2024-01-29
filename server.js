ece1b01215b10bc3b83daf2ca0a14d62c98e3dd8
Prashant170620 <ttipathiprashant300@gmail.com>
2024-01-29 05:32:31 +0530

    All

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { get } = require('@vercel/edge-config');
+const { get ,set} = require('@vercel/edge-config');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,10 +9,15 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/app',async(req,res)=>{
-    var kv = await get();
-    await kv.set('key','value');
+    // var kv = await get();
+    try{
+        await kv.set('key','value');
     var value = await get('key');
-    res.send(`Data from data base ${value}`);  
+    res.send(`Data from data base ${value}`);
+    }  
+    catch(err){
+        res.json(err);
+    }
 })
 
 app.get('*', (req, res) => {
